18b9f403a9c649090d7f16b918b30e285c8fe016
mishabp9633 <mishabp9633@gmail.com>
2023-04-10 23:14:27 +0530

    new

--- routes/product.route.js
+++ routes/product.route.js
@@ -6,24 +6,19 @@ import { getAllProduct, saveProduct,
     } from '../controllers/product.controller.js'
 
 import express from 'express'
-import multer from 'multer'
 
 import { authorizeRoles } from "../middlewares/auth.middleware.js"
 import { ROLES } from "../constants/role.constants.js"
-
-
-const storage = multer.diskStorage({})
-const upload = multer({storage})
 
 const router = express.Router()
 const path = "/product"
 
 // ...........seller...........//
-router.post(`${path}/new`, upload.array('photos'), authorizeRoles([ROLES.SELLER]), saveProduct)
+router.post(`${path}/new`, authorizeRoles([ROLES.SELLER]), saveProduct)
 router.get(`${path}/seller/all`, authorizeRoles([ROLES.ADMIN,ROLES.SELLER]), getAllProductUserByToken)
 router.get(`${path}/seller/all/:id`, authorizeRoles([ROLES.ADMIN,ROLES.SELLER]), getSingleProduct)
 router.put(`${path}/seller/update/:id`, authorizeRoles([ROLES.ADMIN,ROLES.SELLER]), updateProductDataByToken)
-router.put(`${path}/seller/update/photos/:id`, upload.array('photos'),authorizeRoles([ROLES.ADMIN,ROLES.SELLER]), updateProductPhotoByToken)
+router.put(`${path}/seller/update/photos/:id`,authorizeRoles([ROLES.ADMIN,ROLES.SELLER]), updateProductPhotoByToken)
 router.delete(`${path}/seller/delete/:id`, authorizeRoles([ROLES.SELLER]), deleteProductDataByToken)
 
 //...........admin..............//
